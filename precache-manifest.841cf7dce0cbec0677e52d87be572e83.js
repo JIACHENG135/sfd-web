7b9a330674cdef2a35bb491c60ceffb05a5ae3aa
JIACHENG135 <liulaoye135@tamu.edu>
2020-09-15 14:51:04 -0500

    first commit

---
+++ precache-manifest.841cf7dce0cbec0677e52d87be572e83.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cd705c7521cadb77b5811f407947e4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b696f95729fc7db03c18",
+    "url": "/static/css/2.1d4cf820.chunk.css"
+  },
+  {
+    "revision": "833fe372918d7a3b6221",
+    "url": "/static/css/main.778b3489.chunk.css"
+  },
+  {
+    "revision": "b696f95729fc7db03c18",
+    "url": "/static/js/2.28047163.chunk.js"
+  },
+  {
+    "revision": "55d81ee0795cb5e5998c8f480fb87e7a",
+    "url": "/static/js/2.28047163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "833fe372918d7a3b6221",
+    "url": "/static/js/main.a4e77881.chunk.js"
+  },
+  {
+    "revision": "fc412ea9aec2639d86e7",
+    "url": "/static/js/runtime-main.57a13424.js"
+  },
+  {
+    "revision": "f6e910c13591d7a09d897199c019d99e",
+    "url": "/static/media/1.f6e910c1.jpg"
+  },
+  {
+    "revision": "4b97738d30766ba331c548f8e27a3fc7",
+    "url": "/static/media/2.4b97738d.jpg"
+  },
+  {
+    "revision": "b4fea9b9a4ddd31d660f3aa561b2b050",
+    "url": "/static/media/3.b4fea9b9.png"
+  },
+  {
+    "revision": "b02bdc1b846fd65473922f5f62832108",
+    "url": "/static/media/element-icons.b02bdc1b.ttf"
+  },
+  {
+    "revision": "d2f69a92faa6fe990d2e613c358be705",
+    "url": "/static/media/element-icons.d2f69a92.woff"
+  }
+]);
