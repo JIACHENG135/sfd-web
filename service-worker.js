0e80da3b7a19a9b852ca8bb18beb720479068ed0
JIACHENG135 <liulaoye135@tamu.edu>
2020-09-15 23:27:50 -0500

    Change video to SFD

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.841cf7dce0cbec0677e52d87be572e83.js"
+  "/precache-manifest.9ecc910822c638f466ab64e52ad80056.js"
 );
 
 self.addEventListener('message', (event) => {
